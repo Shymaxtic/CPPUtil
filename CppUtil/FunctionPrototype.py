dc95893dc1c5b04693a1a7f9f65b9ae7a6f31e06
shymaxtic <shymaxtic@gmail.com>
2019-11-12 16:17:16 +0700

    1. Update

--- CppUtil/FunctionPrototype.py
+++ CppUtil/FunctionPrototype.py
@@ -1,5 +1,62 @@
+import re
+
 class FunctionPrototype:
+    mConstReturn = ""
+    mReturnType = ""
+    mFunctionName = ""
+    mArgList = []
+    mPrototype = ""
     def __init__(self, prototype: str):
-        pass
+        self.mPrototype = prototype
+        self.__Parse()
+
     def CheckMatch(self, prototype: str):
         pass
+    
+    def __Parse(self):
+        """Parse to get element from protype
+        Ex: 
+        const bool DoSomething(const ArgType1& arg1, const ArgType2& arg2) const
+        const bool DoSomething(const ArgType1** arg1, const ArgType2** arg2) const
+        const bool DoSomething(const ArgType1&& arg1, const ArgType2&& arg2) const
+        const bool DoSomething(const ArgType1* const arg1, const ArgType2* const arg2) const
+        const bool DoSomething(const ArgType1 * const, const ArgType2 * const) const
+        bool DoSomething(ArgType1 &arg1, ArgType2 &arg2)
+        bool DoSomeThing(ArgType1&, ArgType2&)
+        void DoSomeThing()
+       """
+        ret = re.split('\(|\)', self.mPrototype)
+    #    print(ret)
+        functionNameSide = ret[0].split(" ")
+        self.mFunctionName = functionNameSide[-1]
+        self.mReturnType = functionNameSide[-2]
+        if len(functionNameSide) == 3:
+            self.mConstReturn = functionNameSide[-3]
+        argListSide = ret[1].split(",")
+        specificChar = ['**', '&&', '&', '*']
+        for arg in argListSide:
+            argInfo = []
+            for ele in arg.split(" "):
+                doNext = False
+                for speChar in specificChar:
+                    if speChar in ele:
+                        for e in ele.split(speChar):
+                            if e:
+                                argInfo.append(e)
+                        argInfo.append(speChar)
+                        doNext = True
+                        break
+                if doNext == False and ele:
+                    argInfo.append(ele)
+            print(argInfo)                    
+            if argInfo != []:
+                self.mArgList.append(argInfo)                        
+            
+    
+        print("Parsed:{} {} {}".format(self.mConstReturn, self.mReturnType, self.mFunctionName))
+        # print("Arguments:", self.mArgList)
+        for arg in self.mArgList:
+            print(arg)
+
+
+       
--- Test/FunctionPrototype_UT.py
+++ Test/FunctionPrototype_UT.py
@@ -3,16 +3,39 @@ import sys
 sys.path.append("../CppUtil")
 from FunctionPrototype import FunctionPrototype
 
-testPrototype1 = "const bool GetResult(const ClassType&) const"
 class TestCppUtil(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_Prototype_1(self):
-        testObj = FunctionPrototype(testPrototype1)
-        prototype = "const bool GetResult(const ClassType&) const"
+        testObj = FunctionPrototype("const bool DoSomething(const ArgType1& arg1, const ArgType2& arg2) const")
+        prototype = "const bool DoSomething(const ArgType1& arg1, const ArgType2& arg2) const"
         ret = testObj.CheckMatch(prototype)
         self.assertEqual(ret, True)
 
+    # def test_Prototype_2(self):
+    #     testObj = FunctionPrototype("const bool DoSomething(const ArgType1* const arg1, const ArgType2* const arg2) const")
+    #     prototype = "const bool DoSomething(const ArgType1* const arg1, const ArgType2* const arg2) const"
+    #     ret = testObj.CheckMatch(prototype)
+    #     self.assertEqual(ret, True)        
+
+    # def test_Prototype_3(self):
+    #     testObj = FunctionPrototype("bool DoSomething(ArgType1 &arg1, ArgType2 &arg2)")
+    #     prototype = "bool DoSomething(ArgType1 &arg1, ArgType2 &arg2)"
+    #     ret = testObj.CheckMatch(prototype)
+    #     self.assertEqual(ret, True) 
+
+        
+    # def test_Prototype_4(self):
+    #     testObj = FunctionPrototype("bool DoSomeThing(ArgType1&, ArgType2&)")
+    #     prototype = "bool DoSomeThing(ArgType1&, ArgType2&)"
+    #     ret = testObj.CheckMatch(prototype)
+    #     self.assertEqual(ret, True)         
+
+    # def test_Prototype_5(self):
+    #     testObj = FunctionPrototype("void DoSomeThing()")
+    #     prototype = "void DoSomeThing()"
+    #     ret = testObj.CheckMatch(prototype)
+    #     self.assertEqual(ret, True)     
 if  __name__ == '__main__':
     unittest.main()
