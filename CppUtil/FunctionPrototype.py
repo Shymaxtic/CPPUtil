57fcffeba23d52b7ff341ebaf80cf2145eb7d628
shymaxtic <shymaxtic@gmail.com>
2019-11-15 12:42:19 +0700

    license

--- CppUtil/FunctionPrototype.py
+++ CppUtil/FunctionPrototype.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2019 Shymaxtic
+# 
+# This file is part of CPPUtil.
+# 
+# CPPUtil is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# CPPUtil is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with CPPUtil.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import re
 
 class FunctionPrototype:
@@ -30,7 +48,7 @@ class FunctionPrototype:
         bool DoSomeThing(ArgType1&, ArgType2&)
         void DoSomeThing()
        """
-        ret = re.split('\(|\)', self.mPrototype)
+        ret = re.split(r'\(|\)', self.mPrototype)
     #    print(ret)
         functionNameSide = ret[0].split(" ")
         self.mFunctionName = functionNameSide[-1]
